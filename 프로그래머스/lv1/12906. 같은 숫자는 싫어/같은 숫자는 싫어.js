b9704ca63027c25ee38e57247e651b89dfaf3bf7
JinHo Jang <105098581+jsdmas@users.noreply.github.com>
2023-07-10 15:32:36 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 13.74 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -2,6 +2,7 @@ function solution(arr)
 {
     const stack = [];
     for(let i = 0; i < arr.length; i++){
+        // 배열 길이를 벗어나면 push는 되지 않는다.
         if(arr[i] !== arr[i+1]) stack.push(arr[i]);
     }
     return stack;
