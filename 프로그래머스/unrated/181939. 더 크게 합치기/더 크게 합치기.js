f95d65061bf6dc7678d29d8ddeddb6d237280306
JinHo Jang <105098581+jsdmas@users.noreply.github.com>
2023-10-12 07:10:55 +0900

    [level unrated] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
@@ -3,3 +3,8 @@ function solution(a, b) {
     const num2 = String(b) + String(a);
     return +num1 >= +num2 ? +num1 : +num2;
 }
+/*
+function solution(a, b) {
+    return Math.max(Number(`${a}${b}`), Number(`${b}${a}`))
+}
+*/
