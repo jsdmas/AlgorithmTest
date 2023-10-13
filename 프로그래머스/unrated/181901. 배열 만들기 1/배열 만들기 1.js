d0582f2d21767c21a28732f4b2b1a8465b3c1fff
JinHo Jang <105098581+jsdmas@users.noreply.github.com>
2023-10-13 11:55:13 +0900

    [level unrated] Title: 배열 만들기 1, Time: 2.10 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.js
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.js
@@ -5,3 +5,12 @@ function solution(n, k) {
     }
     return answer
 }
+/*
+function solution(n, k) {
+    var answer = [];
+    for(let i = k ; i <= n; i+=k){
+        answer.push(i)
+    }
+    return answer;
+}
+*/
