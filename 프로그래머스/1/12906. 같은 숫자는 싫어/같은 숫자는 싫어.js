f2571cca4c379db3280bd6bdfb7c3cb216810896
JinHo Jang <105098581+jsdmas@users.noreply.github.com>
2023-12-25 15:55:58 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 11.26 ms, Memory: 83.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,9 @@
+function solution(arr){
+    const stack = [];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i - 1] !== arr[i]){
+            stack.push(arr[i]);
+        }
+    }
+    return stack;
+}
