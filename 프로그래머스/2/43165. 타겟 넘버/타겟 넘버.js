f89c8b69d722aa3faf78ccbf444078d95c437dd9
JinHo Jang <105098581+jsdmas@users.noreply.github.com>
2023-12-25 20:31:32 +0900

    [level 2] Title: 타겟 넘버, Time: 1.08 ms, Memory: 35.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,19 @@
+function solution(numbers, target) {
+    let result = 0;
+    
+    const dfs = (index, sum) => {
+        
+        if(index === numbers.length){
+            if(sum === target){
+                result++;    
+            }
+            return;
+        }
+        dfs(index + 1, sum + numbers[index]);
+        dfs(index + 1, sum - numbers[index]);
+    };
+    
+    dfs(0,0);
+    
+    return result;
+}
