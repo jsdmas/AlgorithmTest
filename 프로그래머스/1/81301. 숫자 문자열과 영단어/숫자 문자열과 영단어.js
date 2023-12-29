ec594e77447871d2f7931e55078cb2e31b18e4f8
JinHo Jang <105098581+jsdmas@users.noreply.github.com>
2023-12-29 14:23:22 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,4 +1,20 @@
 function solution(s) {
-    const str = s.replaceAll("one", 1).replaceAll("zero", 0).replaceAll("two",2).replaceAll("three",3).replaceAll("four",4).replaceAll("five",5).replaceAll("six",6).replaceAll("seven",7).replaceAll("eight",8).replaceAll("nine",9);
-    return +str
+     const charSet = {
+        "zero" : 0,
+        "one" : 1,
+        "two" : 2,
+        "three" : 3,
+        "four" : 4,
+        "five" : 5,
+        "six" : 6,
+        "seven" : 7,
+        "eight" : 8,
+        "nine" : 9,
+    }
+
+    for(const key in charSet) {
+        s = s.replaceAll(key, charSet[key]);
+    }
+    
+    return +s;
 }
