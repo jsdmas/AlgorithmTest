f1d0b2f5a9bb6e78f9397f56bbf357646e86b49d
JinHo Jang <105098581+jsdmas@users.noreply.github.com>
2023-12-29 15:03:00 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 14.76 ms, Memory: 41 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,18 @@
+function solution(s) {
+    const result = [];
+    let temp = '';
+    
+    for(let i = 0; i < s.length; i++){
+        // 문자열이 존재하지 않을경우
+        if(temp.indexOf(s[i]) === -1) {
+            temp += s[i];
+            result.push(-1);
+        }else{
+        // 문자열이 존재할 경우 마지막 위치 찾기
+            result.push(i - temp.lastIndexOf(s[i]));
+            temp += s[i]; // temp 갱신
+        }
+    }
+    
+    return result;
+}
