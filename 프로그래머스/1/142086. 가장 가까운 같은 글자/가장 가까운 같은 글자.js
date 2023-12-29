d0c9925b62a2f6398aa2c65c154d1a9261c49139
JinHo Jang <105098581+jsdmas@users.noreply.github.com>
2023-12-29 15:05:44 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 14.62 ms, Memory: 41 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -16,3 +16,15 @@ function solution(s) {
     
     return result;
 }
+/**
+function solution(s) {
+    const hash={};
+
+    return [...s].map((v,i)=>{
+        const result = hash[v] !== undefined ? i - hash[v] : -1;
+        hash[v] = i;
+        return result;
+    });
+    
+}
+*/
