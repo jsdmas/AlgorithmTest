bc69e3fa9b9f1e150679f4d48746978dd27cbc1a
JinHo Jang <105098581+jsdmas@users.noreply.github.com>
2023-12-30 17:43:31 +0900

    [level 1] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -1,10 +1,11 @@
 function solution(a, b, n) {
-    // 받은 콜라 수
-    let change = 0
+    // a : 콜라와 교환하기 위한 병 수, b : 교환될 콜라 개수, n : 가지고있는 빈 병의 개수
+    let coke = 0
     // 교환 가능한 수량이 아닐때까지 반복
     while(n >= a) {
-        change += Math.floor(n/a)*b
-        n = Math.floor(n/a)*b + n%a
+        const bottle = Math.floor(n/a)*b;
+        coke += bottle
+        n = bottle + n % a
     }
-    return change
+    return coke
 }
