2bf0bd550202150f5afd0b62a9f6fc2a4e29bd76
JinHo Jang <105098581+jsdmas@users.noreply.github.com>
2024-04-28 14:32:08 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 'dev/stdin' : './input.txt';
+const INPUT = fs.readFileSync(filePath).toString().trim().split('\n');
+
+function solution() {
+  const [input] = INPUT;
+  console.log(input + '??!');
+}
+
+solution();
+module.exports = solution;
