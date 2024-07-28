82e89cad980f8bf6809a01ba8e879a6e9c6d18e7
JinHo Jang <105098581+jsdmas@users.noreply.github.com>
2024-07-28 15:39:20 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -0,0 +1,28 @@
+function solution(array) {
+    if (array.length === 1) return array[0];
+    
+    const map = new Map();
+    
+    array.forEach(v => {
+        map.set(v, (map.get(v) || 0) + 1);
+    });
+    
+    let maxNum = Number.MIN_SAFE_INTEGER;
+    const maxKeys = [];
+    
+    for (const [key, value] of map) {
+        if (value > maxNum) {
+            maxNum = value;
+            maxKeys.length = 0; 
+            maxKeys.push(key);
+        } else if (value === maxNum) {
+            maxKeys.push(key);
+        }
+    }
+    
+    if (maxKeys.length === 1) {
+        return maxKeys[0];
+    } else {
+        return -1;
+    }
+}
