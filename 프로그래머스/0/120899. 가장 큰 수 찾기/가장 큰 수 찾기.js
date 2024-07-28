21ac5ef0db9477e86b7909df450c86de5c68410b
JinHo Jang <105098581+jsdmas@users.noreply.github.com>
2024-07-28 16:32:30 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,11 +1,4 @@
 function solution(array) {
     if(array.length === 1) return [array[0], 0];
-    let maxNum = 0;
-    let index = 0;
-    
-    array.forEach((v,i)=>{
-        maxNum = Math.max(v, maxNum);
-        if(maxNum === v) index = i;
-    })
-    return [maxNum, index];
+    return [Math.max(...array), array.indexOf(Math.max(...array))];
 }
