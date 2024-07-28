e8063d647c58edccece99e833b7c73bdb0738bf5
JinHo Jang <105098581+jsdmas@users.noreply.github.com>
2024-07-29 08:59:11 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.70 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,3 +1,11 @@
+/**
+function solution(numbers) {
+    numbers.sort((a, b) => a - b);
+    return Math.max(numbers[0]*numbers[1], numbers[numbers.length-1]*numbers[numbers.length-2]);
+}
+
+*/
+
 function solution(numbers) {
     if(numbers.length === 2) return numbers[0] * numbers[1];
     let result = 0;
