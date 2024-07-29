d6b9f75277e2876a08bb9a1a40eb27b5487d7bf1
JinHo Jang <105098581+jsdmas@users.noreply.github.com>
2024-07-29 10:26:37 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,2 +1,2 @@
 
-const solution = (my_string) => [...my_string].filter(v => !isNaN(parseInt(v))).map(v => +v).sort((a,b) => a - b);
+const solution = (my_string) => [...my_string].filter(v => !isNaN(v)).map(v => +v).sort((a,b) => a - b);
