314895ef829062098e12a7626ec2cf4a210c4bca
JinHo Jang <105098581+jsdmas@users.noreply.github.com>
2024-07-29 11:01:18 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.js
+++ 프로그래머스/0/120891. 369게임/369게임.js
@@ -2,3 +2,10 @@ function solution(order) {
     const orderStr = order + "";
     return [...orderStr].filter(v => v === "3"|| v ==="6" || v==="9").length;
 }
+/**
+function solution(order) {
+    var answer = [...order.toString().matchAll(/[3|6|9]/g)].length;
+    return answer;
+}
+
+*/
