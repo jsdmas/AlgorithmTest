cb7b953ae7be35ad702b82e98c48dee0293bbf83
JinHo Jang <105098581+jsdmas@users.noreply.github.com>
2024-08-01 07:31:10 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.js
@@ -1,9 +1,17 @@
 function solution(n) {
     let factorial = 1;
-    let i = 0;
-    while (factorial <= n) {
-        i++;
-        factorial *= i;
+    let count = 0;
+    while(factorial <= n){
+        count++;
+        factorial *= count;
     }
-    return i - 1;
+    return count - 1;
 }
+/**
+처음에 factorial = 1, count = 0
+첫 번째 반복: factorial = 1, count = 1
+두 번째 반복: factorial = 2, count = 2
+세 번째 반복: factorial = 6, count = 3
+네 번째 반복: factorial = 24, count = 4
+다섯 번째 반복: factorial = 120, count = 5
+*/
