5ef46f50541a609ad2290f09733b603883acdd2f
JinHo Jang <105098581+jsdmas@users.noreply.github.com>
2024-08-04 17:47:41 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -11,3 +11,10 @@ function solution(my_str, n) {
     if(str !== '') stack.push(str)
     return stack
 }
+/**
+function solution(my_str, n) {
+    let res = [];
+    for (let i = 0; i < my_str.length; i+=n) res.push(my_str.slice(i, i+n));
+    return res;
+}
+*/
