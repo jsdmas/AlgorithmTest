4ec0584288e4c91e4e4e1b45e8753ee3eff5561b
JinHo Jang <105098581+jsdmas@users.noreply.github.com>
2024-08-04 14:53:30 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -2,14 +2,24 @@ function solution(my_string) {
     const stack = [];
     let temp = '';
     
-    [...my_string, 'x'].forEach((v) => {
+    [...my_string].forEach((v) => {  
         if (!isNaN(v)) {
             temp += v;
-        } else if (temp !== '') {
-            stack.push(parseInt(temp));
+        }else if(temp !== ''){
+            stack.push(+temp);
             temp = '';
         }
     });
     
+    if(temp !== ''){
+        stack.push(+temp);
+    }
+    
     return stack.reduce((acc, cur) => acc + cur, 0);
 }
+
+/**
+function solution(my_string) {
+  return my_string.split(/\D+/).reduce((acc, cur) => acc + Number(cur), 0);
+}
+*/
