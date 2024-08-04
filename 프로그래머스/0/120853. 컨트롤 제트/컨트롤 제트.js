f3d57170d38c2400dc526f68b4189f46bc4cda12
JinHo Jang <105098581+jsdmas@users.noreply.github.com>
2024-08-04 12:52:52 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -1,10 +1,21 @@
-function solution(s) {
-    const calcurator = s.split(' ').reduce((acc,cur,idx,src)=>{
-        if(cur === "Z"){
-            return acc + (-1 * +src[idx - 1])
-        }else{
-            return acc + +cur;
-        }
-    },0);
-    return calcurator;
+function solution(s){
+    const stack = [];
+    
+    s.split(' ').forEach((v)=>{
+        if(v === "Z") stack.pop();
+        else stack.push(+v);
+    });
+    
+    return stack.length ? stack.reduce((acc,cur)=> acc + cur , 0) : 0;
 }
+
+// function solution(s) {
+//     const calcurator = s.split(' ').reduce((acc,cur,idx,src)=>{
+//         if(cur === "Z"){
+//             return acc + (-1 * +src[idx - 1])
+//         }else{
+//             return acc + +cur;
+//         }
+//     },0);
+//     return calcurator;
+// }
